630027e9ab1f5b492b0a29953d7c070f4eb37474
elibon99 <bonnici@kth.se>
2020-02-13 18:00:22 +0100

    Update api.js
    testing deployment on azure

--- routes/api.js
+++ routes/api.js
@@ -6,9 +6,10 @@ const QaModel = require('../models/qa.js');
 //question: req.query.qs
 // get list of answers from db
 router.get('/qa', function(req, res, next){
-  QaModel.find({}).then(function(qa){
-    res.send(qa);
-  })
+  // QaModel.find({}).then(function(qa){
+  //   res.send(qa);
+  // })
+  res.send({type:'GET'});
 });
 
 // add new question to db
