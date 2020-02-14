581009b3b7a83d2fcb829e8185302e482d84d8fd
elibon99 <bonnici@kth.se>
2020-02-14 05:45:35 +0100

    Frontend
    Updated index.html file

--- index.js
+++ index.js
@@ -11,13 +11,15 @@ const qbotapp = express();
 mongoose.connect('mongodb://qabotdb:ryd5jwE2bpnq4nzTafyV7zHwNqFwKsGet9EQjmZ5b8zvlfY59SlLX9BJO6aHfZi35paKR9mMIUzZc8e7V6YjIA%3D%3D@qabotdb.documents.azure.com:10255/mean-dev?ssl=true&sslverifycertificate=false');
 mongoose.Promise = global.Promise;
 
+app.use(express.static('public'));
+
 // intlialize body parser middleware
 qbotapp.use(bodyParser.json());
 
 // initialize routes to be used by app
 qbotapp.use('/api',routes);
 
-// initialize erro-handling middleware
+// initialize error-handling middleware
 qbotapp.use(function(err,req,res,next){
   res.status(422).send({error: err.message});
 });
